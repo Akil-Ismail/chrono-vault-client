f915dd492c210d82c902ab823219335ba6eb72d9
Akil Ismail <akilismailh@gmail.com>
2025-07-15 22:56:33 +0300

    side bar component

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <a href="#" className="nav-link">
+        Public Wall
+      </a>
+      <a href="#" className="nav-link">
+        Personal Wall
+      </a>
+    </aside>
+  );
+};
+
+export default Sidebar;
