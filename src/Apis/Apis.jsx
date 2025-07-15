7bd72be3f65dd0733677557d8f5c40b5d4cc15f5
Akil Ismail <akilismailh@gmail.com>
2025-07-15 22:23:11 +0300

    Apis  and app initiating

---
+++ src/Apis/Apis.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Register from "../Pages/Register/Register";
+import Login from "../Pages/Login/Login";
+import { Routes, Route } from "react-router-dom";
+import Landing from "../Pages/Landing/Landing";
+const Apis = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/" element={<Landing />}></Route>
+    </Routes>
+  );
+};
+
+export default Apis;
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./Styles/App.css";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Apis from "./Apis/Apis";
+
+function App() {
+  return <Apis />;
+}
+
+export default App;
