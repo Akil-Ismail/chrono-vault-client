d050895c6eecd296b78a19dd0a6fe62c1899781c
Akil Ismail <akilismailh@gmail.com>
2025-07-15 22:42:05 +0300

    Login page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LoginForm from "../../Components/LoginForm";
+import "./styles.css";
+
+const Login = () => {
+  return (
+    <div>
+      <header>
+        <h1> ChronoVault </h1>
+      </header>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
